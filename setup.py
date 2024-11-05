3f69db5efa2470319f6b4b16dceb6b5d2f9085ba
Shibo XU <xushibo11@gmail.com>
2024-11-05 19:26:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+# file: setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="seismic_tool",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "seismic_tool=seismic_tool.cli:main",
+        ],
+    },
+    install_requires=[
+        "numpy",
+        "scipy",
+        "obspy"
+    ],
+)
 
